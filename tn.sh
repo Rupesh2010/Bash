f79ca11c6b705f52c67d05c08fcbd4326c2124d1
rphuyal <rupesh.phuyal.ext@nokia.com>
2017-04-25 14:51:09 -0400

    Shell scripts

---
+++ tn.sh
@@ -0,0 +1,5 @@
+#!/opt/exp/bin/bash
+DIR=/prod/lte/pb1/rphuyal/SHELL
+
+
+echo $(date): $* >> ${DIR}/notes.txt
---
+++ wordcount.sh
@@ -0,0 +1,6 @@
+#!/opt/exp/bin/bash
+
+if [[ $# -lt 1 ]]
+then 
+ echo "Usage: $0 file"
+fi
